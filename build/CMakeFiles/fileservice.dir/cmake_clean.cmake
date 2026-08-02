feeccdc489ffc9ec00be24b71d14faa293ed1e3e
Aryan-Das <aryan.das.us@gmail.com>
2026-08-02 00:47:27 +0000

    project skeleton

---
+++ build/CMakeFiles/fileservice.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fileservice.dir/src/main.cpp.o"
+  "CMakeFiles/fileservice.dir/src/main.cpp.o.d"
+  "fileservice"
+  "fileservice.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fileservice.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
